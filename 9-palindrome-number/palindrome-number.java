f9ecaa1b4e659646c694d27c3582cbf2998f2ee2
rrupesh1725-cell <rrupesh1725@gmail.com>
2025-08-30 13:50:55 +0530

    Time: 6 ms (26.12%) | Memory: 44.2 MB (46.73%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,18 +1,16 @@
 class Solution {
-    public boolean isPalindrome(int n) {
-        if(n<0){
-            return false;
+    public boolean isPalindrome(int x) {
+        String s1 = String.valueOf(x);
+        int left = 0;
+        int right = s1.length() - 1;
+        
+        while (left < right) {
+            if (s1.charAt(left) != s1.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-       return n == isreverse(n);
-          
-    }
-    public int isreverse(int x){
-        int reverse = 0;
-        while(x>0){
-           int remainder = x%10;
-           reverse = reverse*10+remainder;
-           x/=10;
-        }
-        return reverse;
+        return true;
     }
 }
