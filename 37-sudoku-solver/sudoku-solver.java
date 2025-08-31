94965c69ab03c4f3ca0a2e8b7703b0366517f2d6
rrupesh1725-cell <rrupesh1725@gmail.com>
2025-08-31 08:44:50 +0530

    Time: 205 ms (63.34%) | Memory: 41.2 MB (56.80%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = '.'; // backtrack
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        // Check row & column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) return false;
+            if (board[i][col] == num) return false;
+        }
+
+        // Check 3x3 sub-box
+        int boxRowStart = (row / 3) * 3;
+        int boxColStart = (col / 3) * 3;
+        for (int r = boxRowStart; r < boxRowStart + 3; r++) {
+            for (int c = boxColStart; c < boxColStart + 3; c++) {
+                if (board[r][c] == num) return false;
+            }
+        }
+
+        return true;
+    }
+}
