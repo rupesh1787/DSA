1e874aa15c1f260a9cefab2b7b3a2dede5360653
rrupesh1725-cell <rrupesh1725@gmail.com>
2025-10-02 10:12:46 +0530

    Time: 7 ms (28.32%) | Memory: 46.3 MB (37.12%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);   // sort the array
-        int n = nums.length;
-
-        // Check pairs of numbers
-        for (int i = 0; i < n - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
-                return nums[i];  // this one is unique
-            }
+        Arrays.sort(nums);
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            result^=nums[i];
         }
-
-        return nums[n - 1];
+        return result;
     }
 }
