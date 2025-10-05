4777f2f8d8fc6fa66be8ea0ad75065f263a06583
rrupesh1725-cell <rrupesh1725@gmail.com>
2025-10-05 12:33:05 +0530

    Time: 3 ms (81.85%) | Memory: 43 MB (25.28%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        StringBuilder sb = new StringBuilder("");
+        String words [] = title.split(" ");
+        for(String word:words){
+            if(word.length()<=2){
+                sb.append(word.toLowerCase());
+
+
+            }
+            else{
+                sb.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase());
+            }
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
