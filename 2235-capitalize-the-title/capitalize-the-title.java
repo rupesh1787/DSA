bc24e7e9b4a594a6886dcd4b113a0fee9308f097
rrupesh1725-cell <rrupesh1725@gmail.com>
2025-10-05 12:42:26 +0530

    Time: 3 ms (81.85%) | Memory: 42.8 MB (40.34%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -1,12 +1,10 @@
 class Solution {
     public String capitalizeTitle(String title) {
         StringBuilder sb = new StringBuilder("");
-        String words [] = title.split(" ");
+        String words[]  = title.split(" ");
         for(String word:words){
             if(word.length()<=2){
                 sb.append(word.toLowerCase());
-
-
             }
             else{
                 sb.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase());
