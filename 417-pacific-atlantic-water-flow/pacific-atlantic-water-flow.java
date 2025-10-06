a886eb6c9d6ce9aa8ac7c7e2f6998ff972c5edbd
rrupesh1725-cell <rrupesh1725@gmail.com>
2025-10-06 07:09:31 +0530

    Time: 4 ms (97.63%) | Memory: 46.3 MB (11.42%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        
+        // DFS from Pacific (top row + left column)
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0, heights[i][0]);
+            dfs(heights, atlantic, i, n - 1, heights[i][n - 1]);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j, heights[0][j]);
+            dfs(heights, atlantic, m - 1, j, heights[m - 1][j]);
+        }
+        
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(Arrays.asList(i, j));
+                }
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int[][] heights, boolean[][] visited, int r, int c, int prevHeight) {
+        int m = heights.length, n = heights[0].length;
+        // Out of bounds or already visited or cannot flow
+        if (r < 0 || c < 0 || r >= m || c >= n || visited[r][c] || heights[r][c] < prevHeight)
+            return;
+        
+        visited[r][c] = true;
+        
+        // 4 directions
+        dfs(heights, visited, r + 1, c, heights[r][c]);
+        dfs(heights, visited, r - 1, c, heights[r][c]);
+        dfs(heights, visited, r, c + 1, heights[r][c]);
+        dfs(heights, visited, r, c - 1, heights[r][c]);
+    }
+}
