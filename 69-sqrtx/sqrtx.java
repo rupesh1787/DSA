6370a8c95fa3a5e89fb5513ae97bd7b74adab79b
Rupesh katariya <rrupesh1725@gmail.com>
2025-10-22 16:04:19 +0530

    Time: 1 ms (85.49%) | Memory: 41.4 MB (13.25%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,7 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        double sqrt = Math.sqrt(x);
-        int floor = (int)Math.floor(sqrt);
-        return floor;
-    }
+        if(x == 0 || x == 1) return x;  
+
+        int start = 1;
+        int end = x / 2;
+        int ans = 0;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            long midSquared = (long) mid * mid;  // avoid overflow
+
+            if(midSquared == x) {
+                return mid;
+            } else if(midSquared < x) {
+                ans = mid;      // store last mid whose square < x
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;  
+}
 }
