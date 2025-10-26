f48b9352e51bd0ea06e857f04c4975efb79cb890
Rupesh katariya <rrupesh1725@gmail.com>
2025-10-26 16:27:36 +0530

    Time: 2 ms (97.64%) | Memory: 56.2 MB (68.12%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,20 +1,24 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total =0; // at the end when we will be travesring the loop agar mera total gas - total cost negative aaye toh return -1
-        int ans = 0;
-        // ek aur vairable rkhunga for checking a particular index fit baith rha ya nhi
-         int index = 0;
-         // track of index
-         for(int i=0;i<gas.length;i++){
-            total+=gas[i]-cost[i];
-            ans+=gas[i]-cost[i];
-            if(ans<0){
-                ans = 0;
-                index = i+1;
+        int totalgas = 0;
+        int totalcost = 0;
+        for(int i=0;i<gas.length;i++){
+            totalgas+=gas[i];
+            totalcost+=cost[i];
+        }
+        if(totalgas-totalcost<0) {
+            return -1;
+        }
 
+        int start =0;
+        int currentgas = 0;
+        for(int i=0;i<gas.length;i++){
+            currentgas+=gas[i]-cost[i];
+            if(currentgas<0){
+                currentgas = 0;
+                start = i+1;
             }
-         }
-         if(index>gas.length || total<0) return -1;
-         return index;
-     }
+        }
+        return start;
+    }
 }
