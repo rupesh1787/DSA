dcbaac1bfad5196896bcc537a8984c2d244bb020
Rupesh katariya <rrupesh1725@gmail.com>
2025-10-29 10:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (47.90%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,41 +1,24 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-           int count = 0;
-           int result = 0;
-           int n = nums.length;
-           for(int i=0;i<n;i++){
-              if(nums[i]!=0){
-                count++;
-              }
-           }
-           for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                if(check(nums,i,1,count)){
-                    result+=1;
-                }
-                if(check(nums,i,-1,count)){
-                    result+=1;
-                }
-            }
-           }
-           return result;
-
- }
-    public boolean check(int nums[],int idx , int dir ,int count){
-        
-        
-        int temp[] = nums.clone();
-        while(idx>=0 && idx<nums.length && count>0){
-            if(temp[idx]>0){
-                temp[idx]--;
-                dir*=-1;
-                if(temp[idx]==0){
-                    count--;
-                }
-            }
-            idx+=dir;
+        int sum = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-        return count == 0;
+        int result = 0;
+        int leftsum = 0;
+        for(int i=0;i<n;i++){
+             leftsum+= nums[i];
+            int rightsum = sum-leftsum;
+            if(nums[i]==0){
+            if(leftsum == rightsum){
+                result+=2;
+            }
+            if(Math.abs(leftsum-rightsum)==1){
+                result+=1;
+            }
+            }
+        }
+        return result;
     }
-
 }
