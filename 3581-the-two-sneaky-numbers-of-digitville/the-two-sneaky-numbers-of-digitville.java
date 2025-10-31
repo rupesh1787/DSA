4df61488a25f1bb736454c48af0059138fa2c4e0
Rupesh katariya <rrupesh1725@gmail.com>
2025-10-31 21:27:24 +0530

    Time: 3 ms (36.86%) | Memory: 46.7 MB (7.71%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,13 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int duplicate[] = new int[2];
-        Arrays.sort(nums);
+        // Step 1: Create a HashMap to count frequencies
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Step 2: Count occurrences of each number
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 3: Find numbers that appear twice
+        int[] result = new int[2];
         int index = 0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                duplicate[index++] = nums[i];
+
+        for (int keys : map.keySet()) {
+            if (map.get(keys) == 2) { // found a sneaky number
+                result[index++] = keys;
             }
         }
-        return duplicate;
+
+        return result;
     }
 }
