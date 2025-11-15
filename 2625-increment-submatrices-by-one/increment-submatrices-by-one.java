f7595574acc8146d71bfc772a226b86eba7583ac
Rupesh katariya <rrupesh1725@gmail.com>
2025-11-15 13:52:35 +0530

    Time: 461 ms (17.19%) | Memory: 77.3 MB (17.19%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int mat[][] = new int[n][n];
+        for(int [] q:queries){
+            int r1 = q[0];
+            int c1 = q[1];
+            int r2 = q[2];
+            int c2 = q[3];
+
+            for(int i=r1;i<=r2;i++){
+                for(int j=c1;j<=c2;j++){
+                    mat[i][j]+=1;
+                }
+            }
+        }
+        return mat;
+    }
+}
+// Time Complexity --> O(n^2*q) , space -->0(n^2);
