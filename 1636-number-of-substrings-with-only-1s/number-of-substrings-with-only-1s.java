9b4a213e81a5f24347677248dbd24f5be97f7671
Rupesh katariya <rrupesh1725@gmail.com>
2025-11-16 09:42:41 +0530

    Time: 4 ms (90.05%) | Memory: 46.4 MB (6.02%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -2,7 +2,7 @@ class Solution {
     public int numSub(String s) {
         int mod = 1_000_000_007;
         long ans  = 0;
-        long count = 0;
+        long count = 0; // why long as we are mutlpying it with again and again so it could overflow 
         int n = s.length();
         for(int i=0;i<n;i++){
             if(s.charAt(i)=='1'){
@@ -13,7 +13,7 @@ class Solution {
                 count = 0;
             }
         }
-        ans = (ans+(count*(count+1)/2))%mod;
+        ans = (ans+(count*(count+1)/2))%mod;// this is for last set of 1 , as we are only adding in ans once we hit 0 , but what if we have 1 in last 
         return (int)ans;
     }
 }
