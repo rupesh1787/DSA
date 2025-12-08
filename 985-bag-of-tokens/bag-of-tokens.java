7700546960203742e4a7e67299f2789560e5a6a4
Rupesh katariya <rrupesh1725@gmail.com>
2025-12-08 20:37:20 +0530

    Time: 6 ms (49.35%) | Memory: 45.1 MB (48.05%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int score = 0;
+        int max = 0;
+        int i = 0;
+        int j = tokens.length-1;
+        while(i<=j){
+            if(power>=tokens[i]){
+                  power-=tokens[i];
+                  score++;
+                  max = Math.max(max , score);
+                  i++;
+            }
+            else if(power<tokens[i] && score>=1){
+                  power+=tokens[j];
+                  score--;
+                  j--;
+
+            }
+            else{
+                return 0;
+            }
+        }
+        return max;
+    }
+}
