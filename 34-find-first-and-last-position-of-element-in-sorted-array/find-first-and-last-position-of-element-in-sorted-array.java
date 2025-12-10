39f1ed9956625428b078cac10357e10721564b2d
Rupesh katariya <rrupesh1725@gmail.com>
2025-12-10 09:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (50.36%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,49 @@
 class Solution {
+
+    int fleftmost (int[] nums , int target , int n){
+        int left = 0;
+        int right = nums.length-1;
+        int leftmost = -1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                leftmost = mid;
+                right = mid-1;
+            }
+            else if(target>nums[mid]){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return leftmost;
+    }
+    int frightmost (int[] nums , int target , int n){
+        int left = 0;
+        int right = nums.length-1;
+        int rightmost = -1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                rightmost = mid;
+                left = mid+1;
+            }
+            else if(target>nums[mid]){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return rightmost;
+    }
     public int[] searchRange(int[] nums, int target) {
-           int out[] = {-1,-1};
-           for(int i=0;i<nums.length;i++){
-              if(nums[i]==target){
-                  out[0] = i;
-                  break;
-              }
-           }
-           for(int j=nums.length-1;j>=0;j--){
-              if(nums[j]==target){
-                out[1] = j;
-                break;
-              }
-           }
-        return out;           
+        int n = nums.length;
+        int leffts = fleftmost(nums,target , n);
+        int rights = frightmost(nums , target , n);
+
+        return new int [] {leffts , rights};
+        
     }
 }
