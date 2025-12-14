7c0cfda16f10993c9c5292746ae591d4fe42134a
Rupesh katariya <rrupesh1725@gmail.com>
2025-12-14 11:21:57 +0530

    Time: 34 ms (27.20%) | Memory: 47.7 MB (53.60%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,35 +1,31 @@
 class Solution {
     public int numberOfWays(String corridor) {
-        long MOD = 1_000_000_007;
-
-        int totalSeats = 0;
-        for (char c : corridor.toCharArray()) {
-            if (c == 'S') totalSeats++;
-        }
-
-        if (totalSeats < 2 || totalSeats % 2 == 1) return 0;
-
-        long ways = 1;
-        int seatCount = 0;
-        int plantCount = 0;
-
-        for (char c : corridor.toCharArray()) {
-            if (c == 'S') {
-                seatCount++;
-
-                // when starting a new section (after first section)
-                if (seatCount == 3) {
-                    ways = (ways * (plantCount + 1)) % MOD;
-                    plantCount = 0;
-                    seatCount = 1; // current 'S' starts next section
-                }
-            } else { // 'P'
-                if (seatCount == 2) {
-                    plantCount++;
-                }
-            }
-        }
-
-        return (int) ways;
+          long MOD = 1_000_000_007;
+          int totalseat = 0;
+          for(char c:corridor.toCharArray()){
+            if(c=='S') totalseat++;
+          }
+          if(totalseat<2 || totalseat%2!=0){
+                return 0;
+          }
+           long ways = 1;
+           int plant = 0;
+           int seat = 0;
+           for(char c:corridor.toCharArray()){
+               if(c=='S') {
+                seat++;
+               }
+               if(seat==3){
+                ways = (ways*(plant+1))%MOD;
+                plant = 0;
+                seat = 1;
+               }
+               else {
+                  if(seat==2 && c=='P'){
+                    plant++;
+                  }
+               }
+           }
+           return (int)ways;
     }
 }
