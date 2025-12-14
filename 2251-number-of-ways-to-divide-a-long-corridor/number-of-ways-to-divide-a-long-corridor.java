068d06d8d9527e3a6326ddcb44e698ce895c704e
Rupesh katariya <rrupesh1725@gmail.com>
2025-12-14 11:22:48 +0530

    Time: 31 ms (39.20%) | Memory: 47.8 MB (48.00%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,31 +1,37 @@
 class Solution {
     public int numberOfWays(String corridor) {
-          long MOD = 1_000_000_007;
-          int totalseat = 0;
-          for(char c:corridor.toCharArray()){
-            if(c=='S') totalseat++;
-          }
-          if(totalseat<2 || totalseat%2!=0){
-                return 0;
-          }
-           long ways = 1;
-           int plant = 0;
-           int seat = 0;
-           for(char c:corridor.toCharArray()){
-               if(c=='S') {
+        long MOD = 1_000_000_007;
+
+        int totalseat = 0;
+        for (char c : corridor.toCharArray()) {
+            if (c == 'S') totalseat++;
+        }
+
+        if (totalseat < 2 || totalseat % 2 != 0) {
+            return 0;
+        }
+
+        long ways = 1;
+        int plant = 0;
+        int seat = 0;
+
+        for (char c : corridor.toCharArray()) {
+            if (c == 'S') {
                 seat++;
-               }
-               if(seat==3){
-                ways = (ways*(plant+1))%MOD;
-                plant = 0;
-                seat = 1;
-               }
-               else {
-                  if(seat==2 && c=='P'){
+
+                if (seat == 3) {
+                    ways = (ways * (plant + 1)) % MOD;
+                    plant = 0;
+                    seat = 1;
+                }
+            } 
+            else { // c == 'P'
+                if (seat == 2) {
                     plant++;
-                  }
-               }
-           }
-           return (int)ways;
+                }
+            }
+        }
+
+        return (int) ways;
     }
 }
