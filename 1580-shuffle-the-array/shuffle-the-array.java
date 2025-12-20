efacb5748e7b942cf045b3416b93c8c15a74b699
Rupesh katariya <rrupesh1725@gmail.com>
2025-12-20 10:37:49 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (13.14%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-          int first[] = Arrays.copyOfRange(nums,0,n);
-          int second[] = Arrays.copyOfRange(nums,n,nums.length);
-          int arr[] = new int[nums.length];
-          for(int i=0;i<nums.length;i++){
-            if(i%2==0){
-                arr[i] = first[i/2];
-            }
-            else{
-                arr[i] = second[i/2];
-            }
-          }
-          return arr;
+        int arr[] = new int[nums.length];
+        int idx = 0;
+        for(int i=0;i<n;i++){
+            arr[idx++] = nums[i];
+            arr[idx++] = nums[i+n];
+        }
+        return arr;
     }
 }
