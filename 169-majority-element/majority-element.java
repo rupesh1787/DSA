070202915f944335791b23adc6085b1575d6380d
Rupesh katariya <rrupesh1725@gmail.com>
2025-12-23 18:11:24 +0530

    Time: 7 ms (44.00%) | Memory: 55.9 MB (10.81%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,9 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer,Integer> occur = new HashMap<>();
-        for(int num:nums){
-            occur.put(num,occur.getOrDefault(num,0)+1);
-
-        }
-        for(Map.Entry<Integer,Integer>entry:occur.entrySet()){
-            if(entry.getValue()>n/2){
-                return entry.getKey();
-            }
-
-        }
-        return -1;
+           Arrays.sort(nums);
+           int n = nums.length;
+           // if a elements n/2 se jyada baar aara , toh ofcourse after sorting wo middle ke pass aayega 
+           // 1,1,1,2,2,2,2 - - 2 
+          return nums[n/2];        
     }
 }
