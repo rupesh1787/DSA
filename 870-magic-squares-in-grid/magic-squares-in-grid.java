687110d4f24602d57fe54a9236849233bea1f9b0
Rupesh Katariya <rrupesh1725@gmail.com>
2025-12-30 20:55:05 +0530

    Time: 1 ms (44.89%) | Memory: 43.8 MB (5.11%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.java
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -1,63 +1,51 @@
 class Solution {
-
-    boolean isMagicSquare(int[][] grid, int r, int c) {
-
-        HashSet<Integer> set = new HashSet<>();
-
-        // 1️Check numbers 1–9 and unique
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                int num = grid[r + i][c + j];
-
-                if (num < 1 || num > 9 || set.contains(num)) {
+    boolean ismagicsquare(int grid[][] , int r , int c){
+        // 1-9 , unique
+        HashSet<Integer>set = new HashSet<>();
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int num = grid[r+i][c+j];
+                if(num<1 || num>9 || set.contains(num)){
                     return false;
+                }else{
+                    set.add(num);
                 }
-                set.add(num);
             }
         }
-
-        // Check row, column, diagonal sums
-        int sum = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
-
-        // Row wise
-        for (int i = 0; i < 3; i++) {
-            if (grid[r + i][c] + grid[r + i][c + 1] + grid[r + i][c + 2] != sum) {
+        // Sum (row , columns , diagonal)
+        int sum = grid[r][c]+grid[r][c+1]+grid[r][c+2];
+        //Rows
+        for(int i=0;i<3;i++){
+            if(grid[r+i][c]+grid[r+i][c+1]+grid[r+i][c+2]!=sum){
                 return false;
             }
         }
-
-        // Column wise
-        for (int i = 0; i < 3; i++) {
-            if (grid[r][c + i] + grid[r + 1][c + i] + grid[r + 2][c + i] != sum) {
+        //Columns
+         for(int i=0;i<3;i++){
+            if(grid[r][c+i]+grid[r+1][c+i]+grid[r+2][c+i]!=sum){
                 return false;
             }
         }
-
-        // Diagonals
-        if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != sum) {
+        //Diagonals
+        if(grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2]!=sum){
             return false;
         }
-
-        if (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != sum) {
+        if(grid[r][c+2]+grid[r+1][c+1]+grid[r+2][c]!=sum){
             return false;
         }
-
         return true;
     }
-
     public int numMagicSquaresInside(int[][] grid) {
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int count = 0;
-
-        for (int i = 0; i <= rows - 3; i++) {
-            for (int j = 0; j <= cols - 3; j++) {
-                if (isMagicSquare(grid, i, j)) {
-                    count++;
-                }
+         int row = grid.length;
+         int col = grid[0].length;
+         int count = 0;
+         for(int i=0;i<=row-3;i++){
+            for(int j=0;j<=col-3;j++){
+                  if(ismagicsquare(grid,i,j)){
+                      count++;
+                  }
             }
-        }
-        return count;
+         }
+         return count ;
     }
 }
