e19b7062401f7617c715f574c4119781b5b53688
Rupesh Katariya <rrupesh1725@gmail.com>
2025-12-30 16:17:21 +0530

    Time: 1 ms (73.61%) | Memory: 46.9 MB (7.40%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -10,3 +10,16 @@ class Solution {
         return k;
     }
 }
+/*
+    int i=0; int j =1;
+    while(j<n){
+        if(nums[i]!=nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+       
+            j++;
+        }
+        return i+1;
+    }
+*/
