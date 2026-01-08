63c0e68e1b62c8c5bb47ea60e0c0b8001d176915
Rupesh Katariya <rrupesh1725@gmail.com>
2026-01-08 21:29:03 +0530

    Time: 1 ms (99.17%) | Memory: 47.4 MB (38.58%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int range = 0;
+        int totaljump = 0;
+        int lastjumpidx = 0;
+        int finishline = nums.length - 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            range = Math.max(range, i + nums[i]);
+
+            if (i == lastjumpidx) {
+                totaljump++;
+                lastjumpidx = range;
+            }
+        }
+        return totaljump;
+    }
+}
