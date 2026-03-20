2525abc874f2fc3bac4c91d1f99fcd5bc2b4a206
Rupesh Katariya <rrupesh1725@gmail.com>
2026-03-20 08:22:57 +0530

    Time: 2 ms (99.30%) | Memory: 119.6 MB (37.62%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -6,14 +6,14 @@ class Solution {
             totalgas+=gas[i];
             totalcost+=cost[i];
         }
-        if(totalgas-totalcost<0) {
+        if(totalgas<totalcost) {
             return -1;
         }
 
         int start =0;
         int currentgas = 0;
         for(int i=0;i<gas.length;i++){
-            currentgas+=gas[i]-cost[i];
+            currentgas+= gas[i]-cost[i];
             if(currentgas<0){
                 currentgas = 0;
                 start = i+1;
