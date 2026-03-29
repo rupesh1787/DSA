48cd294189f5b6dd8d543f2506653d93de3e9db9
Rupesh Katariya <rrupesh1725@gmail.com>
2026-03-29 14:57:31 +0530

    Time: 431 ms (30.58%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select distinct author_id as id
+Select distinct author_id as id 
 from Views
 where author_id = viewer_id
-order by author_id
+order by id;
