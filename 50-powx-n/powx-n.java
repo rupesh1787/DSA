486c1d13e9299c39494fc912d938bcf4db209fa6
Rupesh Katariya <rrupesh1725@gmail.com>
2026-04-03 09:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (28.16%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
+    
+    double power(double x, long n) {
+        // base case
+        if (n == 0) return 1;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    } 
+
     public double myPow(double x, int n) {
-               return Math.pow(x,n);
+        if (n < 0) {
+            return 1 / power(x, -(long)n); // avoid overflow
+        }
+        return power(x, n);
     }
 }
