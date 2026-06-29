daaa1ebebc354b5682aa474fd21d8520d27201a1
Rupesh Katariya <rrupesh1725@gmail.com>
2026-06-29 21:20:49 +0530

    Time: 13 ms (36.34%) | Memory: 46.5 MB (20.66%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+       // s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); 
+       //    s = s.tolowercase().replaceAll("[^a-z0-9]","");
+          s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+       for(int i=0;i<s.length()/2;i++){
+           if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                   return false;
+           }
+       }      
+       return true;
+}
+}
