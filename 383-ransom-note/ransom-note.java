080977bbd9dd00fa4554d1ed3224ff8b47904049
Rupesh Katariya <rrupesh1725@gmail.com>
2026-08-15 14:35:12 +0530

    Time: 1 ms (99.95%) | Memory: 46.4 MB (68.53%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+           int count[] = new int[26];
+           for(char ch:magazine.toCharArray()){
+                    count[ch-'a']++;
+           }
+           for(char ch:ransomNote.toCharArray()){
+                   count[ch-'a']--;
 
-        int[] count = new int[26];
 
-        for (char c : magazine.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        for (char c : ransomNote.toCharArray()) {
-            count[c - 'a']--;
-
-            if (count[c - 'a'] < 0) {
-                return false;
+            if(count[ch-'a']<0){
+                 return false;
             }
-        }
-
-        return true;
+           }
+           return true;
     }
 }
