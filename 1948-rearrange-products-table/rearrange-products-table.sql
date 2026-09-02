1eeed39408867302f0aa739c60b884365c43c693
Rupesh Katariya <rrupesh1725@gmail.com>
2026-09-02 22:15:08 +0530

    Time: 625 ms (35.83%) | Memory: 0B (100.00%) - LeetSync

--- 1948-rearrange-products-table/rearrange-products-table.sql
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -1,25 +1,20 @@
-SELECT 
-    product_id,
-    'store1' AS store, 
-    store1 AS price 
-FROM Products 
-WHERE store1 IS NOT NULL
+# Write your MySQL query statement below
+select product_id ,
+   'store1' as store,
+    store1 as price
+from Products
+where store1 is not null
 
-UNION
+union
+select product_id,
+'store2' as store,
+store2 as price
+from Products
+where store2 is not null
+union
+select product_id,
+'store3' as store,
+store3 as price
+from Products
+where store3 is not null
 
-SELECT 
-    product_id,
-    'store2' AS store, 
-    store2 AS price 
-FROM Products 
-WHERE store2 IS NOT NULL
-
-UNION
-
-SELECT 
-    product_id,
-    'store3' AS store, 
-    store3 AS price 
-FROM Products 
-WHERE store3 IS NOT NULL
-ORDER BY product_id ASC;
