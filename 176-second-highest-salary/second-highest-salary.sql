7c1bd1e91849150f69bde3006ba8de9781ebe3df
Rupesh Katariya <rrupesh1725@gmail.com>
2026-09-22 01:16:23 +0530

    Time: 591 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -2,3 +2,6 @@ select (select  distinct salary
 from Employee 
 order by salary desc 
 limit 1 offset 1 ) as SecondHighestSalary
+  
+
+  # when we use inner query result and it is nothing like 0 , the outer query gives as null
