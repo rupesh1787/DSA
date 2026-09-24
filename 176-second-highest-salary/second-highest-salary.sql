319b5b8692459f1fb46b1c2871a40d3ccf836c37
Rupesh Katariya <rrupesh1725@gmail.com>
2026-09-24 07:56:46 +0530

    Time: 327 ms (32.85%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,14 @@
-select (select  distinct salary  
-from Employee 
-order by salary desc 
-limit 1 offset 1 ) as SecondHighestSalary
+SELECT MAX(e1.salary) AS SecondHighestSalary
+FROM Employee e1
+LEFT JOIN Employee e2
+    ON e1.salary < e2.salary
+WHERE e2.salary IS NOT NULL;
+
+##
+##select (select  distinct salary  
+##from Employee 
+##order by salary desc 
+##limit 1 offset 1 ) as SecondHighestSalary
   
 
   # when we use inner query result and it is nothing like 0 , the outer query gives as null
